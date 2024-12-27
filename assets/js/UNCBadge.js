a16c6dd212df644ce95ef6fbf1dc287193ab22f1
Marcel46509 <88722912+Marcel46509@users.noreply.github.com>
2024-12-28 00:32:58 +0100

    Delete assets/js directory

--- assets/js/UNCBadge.js
+++
@@ -1,5 +0,0 @@
-let UNCCode = `
-<a href="index.html" rel="nofollow" ><img src="assets/images/AsteroidIconBlack.png" class="logos"></img></a>
-`;
-
-document.getElementById("new-UNC").innerHTML = UNCCode;
--- assets/js/footer.js
+++
@@ -1,8 +0,0 @@
-let htmlcode = `
-<div class="footer">
-   <h5>Copyright &copy; AstroTeam, 2020-2023</h5>
-   <h5>Copyright &copy; Wallpaper Belong To - Daniel Olah</h5>
-</div>
-`;
-
-document.getElementById("new-footer").innerHTML = htmlcode;
