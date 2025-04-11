38230a85aecebbc0658f3c8d215152c840d8263e
Marcel46509 <88722912+Marcel46509@users.noreply.github.com>
2025-04-11 21:06:20 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 **Asteroid X - Website**
 - Asteroid X is new **Powerful** Roblox Exploit that can run any script you want, our current supported script percentage is 98%!
-- [Asteroid X - Website](https://marcel46509.github.io/astroteam)
+- [Asteroid X - Website](https://marcel46509.github.io/astroteam/)
--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -6,7 +6,7 @@ let navbarcode = `
 <li><a href="index.html" rel="nofollow">Home</a></li>
 <li><a href="Download.html" rel="nofollow">Download</a></li>
 <li><a href="About.html" rel="nofollow">About</a></li>
-<li><a href="https://discord.gg/dy5MfKxmAM" target="_blank" rel="nofollow">Community</a></li>
+<li><a href="https://discord.com/invite/3CAMEqBmH3" target="_blank" rel="nofollow">Community</a></li>
 
 <!-- <li><a href="assets/storage/Terms.html" target="_blank" rel="nofollow">Terms</a></li> -->
 <!-- <li><a href="assets/storage/Privacy Policy.html" target="_blank" rel="nofollow">Privacy</a></li> -->
